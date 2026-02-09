8a31284de33fe05fed44a72ad2b3d9e6a7271d08
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2026-02-09 10:43:29 -0300

    Firebase, Flutterfire y Firestore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    
+}
+
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.9.0"))
 }
 
 android {
@@ -21,7 +27,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_application_1"
+        applicationId = "com.jio.rate_all"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
