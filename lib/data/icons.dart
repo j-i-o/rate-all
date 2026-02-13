2dce28ca5656d2291c28f0898481bf6e6a7c1d2a
Juan I. Ordoqui <juan.ordoqui@gba.gob.ar>
2026-02-13 19:38:12 -0300

    Actualizados íconos

--- lib/data/icons.dart
+++ lib/data/icons.dart
@@ -3,16 +3,22 @@ import 'package:flutter/material.dart';
 class CategoryIcons {
   static const List<IconData> iconos = [
     Icons.star_rounded,
-    Icons.ac_unit_rounded,
+    Icons.circle_rounded,
+    Icons.person_rounded,
+    Icons.local_gas_station_rounded,
+    Icons.flatware_rounded,
+    Icons.coffee_rounded,
+    Icons.bakery_dining_rounded,
     Icons.local_pizza_rounded,
-    Icons.card_travel_rounded,
-    Icons.local_florist_rounded,
-    Icons.tv_rounded,
     Icons.cookie_rounded,
     Icons.icecream_rounded,
-    Icons.airplanemode_active_rounded,
-    Icons.coffee_rounded,
+    Icons.card_travel_rounded,
+    Icons.local_florist_rounded,
+    Icons.park_rounded,
+    Icons.tv_rounded,
     Icons.movie_rounded,
+    Icons.airplanemode_active_rounded,
     Icons.sports_soccer_rounded,
+    Icons.sports_esports_rounded,
   ];
 }
